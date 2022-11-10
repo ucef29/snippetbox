5ffd8944f72fc4aec6ebe33088e32443fcda08f1
ucef29 <yousseflamani@gmail.com>
2022-11-10 10:23:35 -0700

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"ucef29/snippetbox/pkg/models/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Define a new command-line flag for the Mysql DSN string
+	dsn := flag.String("dsn", "web:pass@tcp(192.168.2.227:3306)/snippetbox?parseTime=true", "MySQL data source name")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	//err := http.ListenAndServe(*addr, mux)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
